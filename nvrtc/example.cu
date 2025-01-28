873a4aa2331f20861144396f5900a266d9241b37
ardubois <ardubois@github.com>
2025-01-28 14:49:40 -0300

    teste nvrtc

--- nvrtc/example.cu
+++ nvrtc/example.cu
@@ -122,66 +122,7 @@ if((id < size))
 
 }
 
-extern "C" void map_ske_call(ErlNifEnv *env, const ERL_NIF_TERM argv[], ErlNifResourceType* type,ErlNifResourceType* ftype)
-  {
 
-    ERL_NIF_TERM list;
-    ERL_NIF_TERM head;
-    ERL_NIF_TERM tail;
-
-   // void **fun_res;
-
-    const ERL_NIF_TERM *tuple_blocks;
-    const ERL_NIF_TERM *tuple_threads;
-    int arity;
-
-    if (!enif_get_tuple(env, argv[1], &arity, &tuple_blocks)) {
-      printf ("spawn: blocks argument is not a tuple");
-    }
-
-    if (!enif_get_tuple(env, argv[2], &arity, &tuple_threads)) {
-      printf ("spawn:threads argument is not a tuple");
-    }
-    int b1,b2,b3,t1,t2,t3;
-
-    enif_get_int(env,tuple_blocks[0],&b1);
-    enif_get_int(env,tuple_blocks[1],&b2);
-    enif_get_int(env,tuple_blocks[2],&b3);
-    enif_get_int(env,tuple_threads[0],&t1);
-    enif_get_int(env,tuple_threads[1],&t2);
-    enif_get_int(env,tuple_threads[2],&t3);
-
-    dim3 blocks(b1,b2,b3);
-    dim3 threads(t1,t2,t3);
-
-    list= argv[3];
-
-  int **array_res1;
-    enif_get_list_cell(env,list,&head,&tail);
-    enif_get_resource(env, head, type, (void **) &array_res1);
-    int *arg1 = *array_res1;
-    list = tail;
-
-    int **array_res2;
-    enif_get_list_cell(env,list,&head,&tail);
-    enif_get_resource(env, head, type, (void **) &array_res2);
-    int *arg2 = *array_res2;
-    list = tail;
-
-    enif_get_list_cell(env,list,&head,&tail);
-  int arg3;
-  enif_get_int(env, head, &arg3);
-  list = tail;
-
-   map_ske<<<blocks, threads>>>(arg1,arg2,arg3);
-    cudaError_t error_gpu = cudaGetLastError();
-    if(error_gpu != cudaSuccess)
-     { char message[200];
-       strcpy(message,"Error kernel call: ");
-       strcat(message, cudaGetErrorString(error_gpu));
-       enif_raise_exception(env,enif_make_string(env, message, ERL_NIF_LATIN1));
-     }
-}
 )%%%";
 
 int main() {
