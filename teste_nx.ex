859c92b8671f4892c0504614936e62bbe363cb0e
Andre Rauber Du Bois <rauberdubois@gmail.com>
2025-01-29 17:47:34 -0300

    teste anonymous functions

--- teste_nx.ex
+++ teste_nx.ex
@@ -1,17 +1,17 @@
 require Hok
 
 Nx.tensor([[1,2,3,4]],type: {:s, 32})
-  |> Hok.new_gnx
-  |> Hok.get_gnx
+  |> PHok.new_gnx
+  |> PHok.get_gnx
   |> IO.inspect
 
 Nx.tensor([[1,2,3,4]],type: {:f, 32})
-  |> Hok.new_gnx
-  |> Hok.get_gnx
+  |> PHok.new_gnx
+  |> PHok.get_gnx
   |> IO.inspect
 
 
 Nx.tensor([[1,2,3,4]],type: {:f, 64})
-  |> Hok.new_gnx
-  |> Hok.get_gnx
+  |> PHok.new_gnx
+  |> PHok.get_gnx
   |> IO.inspect
