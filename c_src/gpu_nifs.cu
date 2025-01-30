1d65671731b178c561644c92c679d104e25af53e
Andre Rauber Du Bois <rauberdubois@gmail.com>
2025-01-30 11:24:26 -0300

    teste anonymous functions

--- c_src/gpu_nifs.cu
+++ c_src/gpu_nifs.cu
@@ -16,7 +16,7 @@ void init_cuda(ErlNifEnv *env)
     {
        CUresult err;
        int device = 0;
-
+       printf("Aqui!!!!!!!!!!11\n");
        cuInit(0);
        err = cuCtxCreate(&context, 0, device);
        if(err != CUDA_SUCCESS)  
