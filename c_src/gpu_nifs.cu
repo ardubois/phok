4cc2cfe0ff9f29eae4b19dbff5e53667a4795859
Andre Rauber Du Bois <rauberdubois@gmail.com>
2025-01-31 13:35:27 -0300

    teste anonymous functions

--- c_src/gpu_nifs.cu
+++ c_src/gpu_nifs.cu
@@ -217,6 +217,7 @@ static ERL_NIF_TERM jit_compile_and_launch_nif(ErlNifEnv *env, int argc, const E
 
   init_cuda(env);
 
+  err = cuCtxCreate(&context, 0, device);
   err = cuModuleLoadDataEx(&module,  ptx, 0, 0, 0);
   if (err != CUDA_SUCCESS) fail_cuda(env,err,"cuModuleLoadData jit compile");
 
