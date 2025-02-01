0f126a54f1e42db12617923a9d0d7b9f649839a1
Andre Rauber Du Bois <rauberdubois@gmail.com>
2025-02-01 02:13:36 -0300

    teste anonymous functions

--- teste.ex
+++ teste.ex
@@ -54,6 +54,7 @@ func = Hok.hok fn (x) -> x + 1 end
 prev = System.monotonic_time()
 #r = PMap.map(gtensor,func)
 r = Hok.new_gnx(1,4,{:s,32})
+IO.inspect r
 r = Hok.get_gnx(r)
 IO.inspect r
 next = System.monotonic_time()
