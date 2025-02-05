7e67c7eef424b0b15d6fb7d924e27bf554760262
ardubois <ardubois@github.com>
2025-02-05 10:31:51 -0300

    teste anonymous functions

--- lib/hok.ex
+++ lib/hok.ex
@@ -274,7 +274,7 @@ def get_gnx({:matrex, ref, {rows,columns}}) do
   %Matrex{data: array}
 end
 def get_gnx({:nx, type, shape, name , ref}) do
-  IO.puts "aqui..."
+  #IO.puts "aqui..."
   {l,c} = shape
   ref = case type do
     {:f,32} -> get_gpu_array_nif(ref,l,c,Kernel.to_charlist("float"))
