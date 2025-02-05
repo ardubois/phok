281c9f8685dec8f65581f0c9bfa1f55575fa01c9
ardubois <ardubois@github.com>
2025-02-05 10:49:32 -0300

    teste anonymous functions

--- benchmarks/mm.ex
+++ benchmarks/mm.ex
@@ -45,8 +45,8 @@ m = String.to_integer(arg)
 #mat1 = Matrex.new(1, m*m, fn -> :rand.uniform(1000) end)
 #mat2 = Matrex.new(1, m*m, fn -> :rand.uniform(1000) end)
 
-mat1 = Hok.new_nx_from_function(1,m*m,{:f,32},:rand.uniform(1000) )
-mat2 = Hok.new_nx_from_function(1,m*m,{:f,32},:rand.uniform(1000) )
+mat1 = Hok.new_nx_from_function(1,m*m,{:f,32},fn -> :rand.uniform(1000) end )
+mat2 = Hok.new_nx_from_function(1,m*m,{:f,32},fn -> :rand.uniform(1000) end)
 
 prev = System.monotonic_time()
 
