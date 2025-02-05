154f52e69c8d54b66ed1a8579d21603834d9b113
ardubois <ardubois@github.com>
2025-02-05 12:29:48 -0300

    teste anonymous functions

--- lib/hok/JIT.ex
+++ lib/hok/JIT.ex
@@ -312,7 +312,7 @@ defp process_definitions(module_name,[h|t]) do
                                       #  IO.inspect "Process definitions: #{fname}"
 
 
-                                        body = JIT.add_return(body)
+                                        body = Hok.TypeInference.add_return(body)
                                         funs = find_functions({:defh , ii, [header,[body]]})
                                        # IO.inspect "Function graph: #{inspect funs}"
                                        # IO.inspect "body: #{inspect body}"
