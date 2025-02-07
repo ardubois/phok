05a0775d7fedd3fb23dcf641028694ea52a4a2b8
Andre Rauber Du Bois <rauberdubois@gmail.com>
2025-02-07 12:19:51 -0300

    teste anonymous functions

--- benchmarks/raytracer_ske.ex
+++ benchmarks/raytracer_ske.ex
@@ -90,7 +90,7 @@ defk mapxy_2D_step_2_para_no_resp_kernel(d_array,  step, par1, par2,size,f) do
 end
 def mapxy_2D_para_no_resp(d_array,  step,par1, par2, size, f) do
 
-    Hok.spawn_jit(&NBodies.map_step_2_para_no_resp_kernel/6,{trunc(size/16),trunc(size/16),1},{16,16,1},[d_array,step,par1,par2,size,f])
+    Hok.spawn_jit(&RayTracer.mapxy_2D_step_2_para_no_resp_kernel/6,{trunc(size/16),trunc(size/16),1},{16,16,1},[d_array,step,par1,par2,size,f])
     d_array
 end
 
